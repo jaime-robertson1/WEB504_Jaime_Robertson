c0a9ebf845c00df1f540beb7fc319689ddfb2541
jaime-robertson1 <jaime-robertson@live.nmit.ac.nz>
2025-11-13 00:00:14 +1300

    Have the comments functionailty working so users can upload their comments and they are shown in firestore

--- portfolio-project/components/comments/comments.js
+++ portfolio-project/components/comments/comments.js
@@ -1,7 +1,16 @@
 import { auth, provider, db } from "./firebaseConfig.js";
 import { signInWithPopup, GoogleAuthProvider, signOut } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
+import {
+    collection,
+    addDoc,
+    serverTimestamp
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js"
+
+
 auth.languageCode = 'en'
+
+let currentUser = null;
 
 const googleLogin = document.getElementById("google-login-button");
 googleLogin.addEventListener("click", function(){
@@ -24,3 +33,21 @@ googleLogout.addEventListener("click", function(){
     signOut(auth)
     window.location.href="comments.html";
 })
+
+const commentInput = document.getElementById("comment-input");
+const submitComment = document.getElementById("submit-comment");
+
+submitComment.addEventListener("click", async () => { 
+    const user = auth.currentUser || currentUser;
+    const text = commentInput.value.trim();
+    try {
+        await addDoc(collection(db, "comments"), {
+            text,
+            user: user.displayName,
+            time: serverTimestamp()
+        });
+        commentInput.value = "";
+        } catch (error) {
+            console.error("Error adding comment:", error);
+        }
+    });
