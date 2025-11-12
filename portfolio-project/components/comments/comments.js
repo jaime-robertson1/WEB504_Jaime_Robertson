a8f8a8bcbb1be38cb8080dc49630872503f9c00c
jaime-robertson1 <jaime-robertson@live.nmit.ac.nz>
2025-11-12 14:22:33 +1300

    Making sure login is functional and making a seperate html page which will show the comments only when a user is logged in. Starting to add logout functionality

--- portfolio-project/components/comments/comments.js
+++ portfolio-project/components/comments/comments.js
@@ -1,22 +1,20 @@
 import { auth, provider, db } from "./firebaseConfig.js";
-import { signInWithPopup } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { signInWithPopup, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
-const auth = getAuth(app);
 auth.languageCode = 'en'
-const provider = new GoogleAuthProvider();
 
 const googleLogin = document.getElementById("google-login-button");
 googleLogin.addEventListener("click", function(){
+    provider.setCustomParameters({ prompt: "select_account"});
     signInWithPopup(auth, provider)
     .then((result) => {
         const credential = GoogleAuthProvider.credentialFromResult(result);
         const user = result.user;
         console.log(user);
-        window.location.href="comments.html";
+        window.location.href="loggedin-comments.html";
 
     }) .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
+        console.error("Sign-in error:", error.code, error.message);
     });
 
 })
